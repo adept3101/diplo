fa21f8389349e95b6d3e2eb1de0a9f4b3657fe20
adept3101 <timofeysitnikov@mail.ru>
2025-11-28 14:12:53 +0300

    main

--- back/main.py
+++ back/main.py
@@ -1,13 +1,8 @@
-from typing import Annotated
+from fastapi import FastAPI
 
-from fastapi import Depends, FastAPI
-from fastapi.security import OAuth2PasswordBearer
+# from back.api import course
+from api import course
 
 app = FastAPI()
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-
-@app.get("/items/")
-async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
-    return {"token": token}
+app.include_router(course.router)
