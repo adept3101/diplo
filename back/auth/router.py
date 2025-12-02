634138576495022cbf85ebd3299bf352e403ddbd
adept3101 <timofeysitnikov@mail.ru>
2025-12-02 22:00:29 +0300

    upd

--- back/auth/router.py
+++ back/auth/router.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI, Response, HTTPException, Depends, APIRouter
 from models.schemas import UserSchema
 from auth.aut import security, config
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from typing import Annotated
+from db import get_db
+from models.models import Users
+from sqlalchemy.orm import Session
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+@router.get("/items")
+async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
+    return {"token": token}
 
 
 @router.post("/login")
@@ -16,8 +29,16 @@ def login(creds: UserSchema, response: Response):
 
 @router.get(
     "/protected",
-    tags=["Авторизация"],
     dependencies=[Depends(security.access_token_required)],
 )
 async def protected():
     return {"data": "TOP SECRET"}
+
+
+@router.post("/register")
+def register(reg: UserSchema, db: Session = Depends(get_db)):
+    new_usr = Users(login=reg.login, password=reg.password)
+    db.add(new_usr)
+    db.commit()
+    db.refresh(new_usr)
+    return new_usr
