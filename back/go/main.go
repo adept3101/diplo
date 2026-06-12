1bebf3472eb334c3bbbbde7702e060535c9f4cfa
adept <timofeysitnikov@mail.ru>
2026-06-12 12:47:12 +0300

    go begin

--- back/go/main.go
+++ back/go/main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
-	"api/db"
-	_ "fmt"
-
+	"fmt"
+	"net/http"
 )
 
-func main(){
-	db.Conn()
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, `{"message": "Hello, World! Welcome to Go REST API"}`)
+}
+
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, `{"message":"test"}`)
+}
+
+func main() {
+	// mux := http.NewServeMux()
+
+	// mux.HandleFunc("GET /", helloHandler)
+	// mux.HandleFunc("GET /test", testHandler)
+	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/test", testHandler)
+
+	fmt.Println("Server is listening...")
+	http.ListenAndServe("localhost:8080", nil)
 }
